d2c4e6e65f2a6e4c3914370562693a82f09859fb
varygin-design <39562139+varygin-design@users.noreply.github.com>
2019-06-01 09:25:56 +0700

    add interactive map

--- js/index.js
+++ js/index.js
@@ -19,10 +19,38 @@ window.onload = function () {
             } else if (this.classList.contains('kemerovo')) {
                 div.innerHTML = '<object type="text/html" data="index.html"></object>'
                 div.style.display = 'block'
-            } else if (this.classList.contains('object')) {
+            } else if (this.classList.contains('vynogradovsky')) {
                 div.innerHTML = '<object type="text/html" data="vynogradovsky.html"></object>'
                 div.style.display = 'block'
+            } else if (this.classList.contains('alekceev')) {
+                div.innerHTML = '<object type="text/html" data="alekceev.html"></object>'
+                div.style.display = 'block'
+            } else if (this.classList.contains('chernigovec')) {
+                div.innerHTML = '<object type="text/html" data="chernigovec.html"></object>'
+                div.style.display = 'block'
+            } else if (this.classList.contains('gramotein')) {
+                div.innerHTML = '<object type="text/html" data="gramotein.html"></object>'
+                div.style.display = 'block'
+            } else if (this.classList.contains('polosuh')) {
+                div.innerHTML = '<object type="text/html" data="polosuh.html"></object>'
+                div.style.display = 'block'
+            } else if (this.classList.contains('shlistvyag')) {
+                div.innerHTML = '<object type="text/html" data="shlistvyag.html"></object>'
+                div.style.display = 'block'
+            } else if (this.classList.contains('shugnaya')) {
+                div.innerHTML = '<object type="text/html" data="shugnaya.html"></object>'
+                div.style.display = 'block'
+            } else if (this.classList.contains('shumayskoe')) {
+                div.innerHTML = '<object type="text/html" data="shumayskoe.html"></object>'
+                div.style.display = 'block'
+            } else if (this.classList.contains('karta-vod')) {
+                console.log('object');
+                div.innerHTML = '<object type="text/html" data="interactive_map/main.html"></object>'
+                div.style.display = 'block'
+            } else {
+                
             }
+            
 
 
             // div.innerHTML = '<object type="text/html" data="vynogradovsky.html" ></object>'
@@ -55,10 +83,10 @@ window.onload = function () {
 
     function checkIndex() {
         if (html.classList.contains('index')) {
-            // div.innerHTML = '<div style="position:absolute; top:5px; right:50%;">Hello</div>'
+            div.innerHTML = '<div style="position:absolute; top:5px; right:50%;">Hello</div>'
             div.innerHTML = '<h1 class="object-header">Кемеровская область</h1><div class="navigation-bar"><a href="#openModal" class="objects" id="passport">Паспорт объекта</a><a href="#openModal" class="objects" id="karta">Дорожная карта</a><div id="openModal" class="modalDialog"><div class="modal-container"><a href="#close" title="Закрыть" class="close">X</a><img src="img/vinograd/passport/passVinograd.jpg" class="map-obj pass"></div></div></div><div id="map"><img src="img/map/karta-ko.jpg" alt="" class="map-obj main-map"></div><h1>Привет</h1>'
             div.style.display = 'block'
-            
+
         }
     }
 
